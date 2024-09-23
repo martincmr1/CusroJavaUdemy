6bc2a6742381feb85537a172e4dd23fdd624b3e9
martincmr1 <martincmr1@hotmail.com>
2024-09-23 16:41:46 -0300

    "cursojava"

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nombre;
+        int a, b, c;
+        nombre = "Martin";
+        int edad = 25;
+        a = 1;
+        b = 2;
+        c = 3;
+
+        System.out.println(nombre);
+        System.out.println(edad);
+
+        edad = 26;
+        System.out.println(edad);
+        System.out.println(a + b + c);
+
+        var primerNombre = "Carlos ";
+        var segundoNombre = "Martin";
+        var misNombres = primerNombre + segundoNombre;
+        System.out.println(misNombres);
+
+    }
+}
