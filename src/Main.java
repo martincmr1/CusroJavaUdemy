1ea5e9374ecc9486c06d551bdb35c2ea3606a89b
martincmr1 <martincmr1@hotmail.com>
2024-09-23 17:57:29 -0300

    "operadoresmatematicos"

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,57 @@ public class Main {
         var segundoNombre = "Martin";
         var misNombres = primerNombre + segundoNombre;
         System.out.println(misNombres);
+        
+        //byte edad = 25;
+        //System.out.println(edad);
+        
+        long numeroGrande = 45667455L;
+        System.out.println(numeroGrande  );
+        
+        float nd= 3.5f;
+        
+        System.out.println(nd);
+        
+        double ndd = 3.55d ;
+        System.out.println(ndd);
+        
+        char letra = 'a';
+        char codigo = 64;
+        
+        System.out.println(letra);
+        System.out.println(codigo);
+        
+        boolean opcion = true;
+        opcion = false;
+        
+        String nombreCompleto= "martin rugnia";
+        System.out.println("");
+        
+        int g =90;
+        int h = 6;
+        int i = 5;
+        
+        System.out.println(g%h);
+        System.out.println((g-h * i)/4);
+        
+        a=10;
+        
+        a +=5;
+        
+        System.out.println(a);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
     }
 }
